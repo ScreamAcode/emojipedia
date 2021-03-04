995ba45a47556b5c6587f20560ad6957561621f3
ScreamAcode <saravanasurya100@gmail.com>
2021-03-04 11:39:25 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,109 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😁",
+    name: "Smiling Face With Open Mouth And Smiling Eyes",
+    meaning:
+      "Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm."
+  },
+  {
+    id: 5,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon."
+  },
+  {
+    id: 6,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015."
+  },
+  {
+    id: 7,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 8,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 9,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A light smile means you are satisfied with yourself and the world. Can make a statement sound friendlier than it might have been meant. Or have a sarcastic meaning when a smile does not actually fit the content."
+  },
+  {
+    id: 10,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 11,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  },
+  {
+    id: 12,
+    emoji: "😚",
+    name: "Kissing Face With Closed Eyes",
+    meaning:
+      "Cute face with closed eyes and rosy cheeks. As a thank you for a tip or a favor. Loving kiss to the closest friends, family or darling."
+  },
+  {
+    id: 13,
+    emoji: "🤨",
+    name: "Face With a Raised Eyebrow",
+    meaning:
+      "With the questioning look on the face, resembles the actor “The Rock“ (Dwayne Johnson). Can be used to express skepticism, disbelief or disapproval."
+  },
+  {
+    id: 14,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning:
+      "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval."
+  },
+  {
+    id: 15,
+    emoji: "😟",
+    name: "Worried Face",
+    meaning:
+      "Worried looking face due to a complicated situation or important event. Feels anxious, insecure and uncomfortable."
+  }
+];
+
+export default emojipedia;
